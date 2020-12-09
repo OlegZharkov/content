debaa698e5f7b2e603ab3b7d3efd168b5e793535
Oleg Zharkov <jos.to.new@gmail.com>
2020-12-09 21:24:20 +0100

    fix branch name

--- scripts/git-utils/import-changes.sh
+++ scripts/git-utils/import-changes.sh
@@ -6,12 +6,13 @@ git config --local user.email "tpe-bot@github.com"
 git config --local user.name "Tools Platform Ecosystem bot"
 git add .
 
-if git commit -m "bioconda import on $(date)"; then
+if git commit -m "$2 on $(date)"; then
   if $1; then
     git push
   else
-    git checkout -b $2-$(date +%s%N | cut -b10-19)
-    git push --set-upstream origin "import-$(date +%s%N | cut -b10-19)"
+    BRANCH_NAME=$2-$(date +%s%N | cut -b10-19)
+    git checkout -b $BRANCH_NAME
+    git push --set-upstream origin $BRANCH_NAME
     hub pull-request -m "$2 on $(date)" -l "$2"
   fi
 else
